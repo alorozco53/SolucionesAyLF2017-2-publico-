3aeaf075cc18b24880c6a7934ffd2054037c8c50
AlOrozco53 <alorozco.patriot53@gmail.com>
2017-06-02 16:58:30 -0500

    [ADDED] some functions for turing machines

--- src/turing_machine.hs
+++ src/turing_machine.hs
@@ -1,4 +1,3 @@
-{-# LANGUAGE FlexibleInstances #-}
 {-- Práctica 3
 -- Autómatas y Lenguajes Formales
 -- 2017-2
@@ -7,7 +6,6 @@
 -- Ayudante: Cenobio Moisés Vázquez Reyes
 -- Version: 0.1
 -- Author1: AlOrozco53
--- Author2: <NOMBRE_DE_LOS_INTEGRANTES_DEL_EQUIPO>
 --}
 
 module TM where
@@ -18,10 +16,12 @@ type Estado = [Char]
 type Alfabeto = [Simbolo]
 
 -- | Ejercicio 1, TM motion values
--- data Movimiento = ... deriving (Show, Eq)
+data Movimiento = L -- izquierda
+                | R -- derecha
+                deriving (Show, Eq)
 
 -- | Ejercicio 2, transition function type alias
--- type Delta = ...
+type Delta = Estado -> Simbolo -> (Estado, Simbolo, Movimiento)
 
 -- | Turing machine structure
 data MaqT = MaqT { q :: [Estado],  -- conjunto de estados
@@ -33,7 +33,9 @@ data MaqT = MaqT { q :: [Estado],  -- conjunto de estados
                  } deriving Show
 
 -- | Turing machine definition
-type MT = (MaqT, Delta)
+data MT = MT { mtupla :: MaqT,  -- Turing machine tuple structure
+               dltfun :: Delta  -- transition function
+             }
 
 
 -- | Show functions for standard Turing Machines
@@ -58,7 +60,6 @@ pintaDeltaAux :: Delta -> [(Estado, Simbolo)] -> String
 pintaDeltaAux _ [] = "\n"
 pintaDeltaAux t ((e, s):xs) =
   case t e s of
-    -- ("qr", s', m) -> pintaDeltaAux t xs
       (e', s', m) -> "                        d " ++
                      e ++ " " ++ [s] ++ " = " ++ " " ++e' ++
                      " " ++ [s'] ++ " " ++ show m ++ "\n" ++ pintaDeltaAux t xs
@@ -66,30 +67,180 @@ pintaDeltaAux t ((e, s):xs) =
 pintaDelta :: [Estado] -> Alfabeto -> Delta -> String
 pintaDelta le ls t = pintaDeltaAux t (generaPares le ls)
 
-
 instance Show MT where
-  show (m, d) = "\nEstados:: "
-                ++ pintaEstados (q m) ++ "\n" ++
-                "\nEstado Inicial:: "
-                ++ pintaEstado (q0 m) ++ "\n" ++
-                "\nEstado de Aceptación:: "
-                ++ pintaEstado (qf m) ++ "\n" ++
-                "\nEstado de Rechazo:: "
-                ++ pintaEstado (qr m) ++ "\n" ++
-                "\nAlfabeto de Entrada:: "
-                ++ pintaAlfabeto (s m) ++ "\n" ++
-                "\nAlfabeto de la Cinta:: "
-                ++ pintaAlfabeto (g m) ++ "\n" ++
-                "\nFuncion de Transicion::\n"
-                ++ pintaDelta (q m) (g m) d
+  show mt = "\nEstados:: "
+            ++ pintaEstados (estados mt) ++ "\n" ++
+            "\nEstado Inicial:: "
+            ++ pintaEstado (estadoInicial mt) ++ "\n" ++
+            "\nEstado de Aceptación:: "
+            ++ pintaEstado (estadoAcept mt) ++ "\n" ++
+            "\nEstado de Rechazo:: "
+            ++ pintaEstado (estadoRechazo mt) ++ "\n" ++
+            "\nAlfabeto de Entrada:: "
+            ++ pintaAlfabeto (sigma mt) ++ "\n" ++
+            "\nAlfabeto de la Cinta:: "
+            ++ pintaAlfabeto (gamma mt) ++ "\n" ++
+            "\nFunción de Transición::\n"
+            ++ pintaDelta (estados mt) (gamma mt) d
+    where
+      d = funTransicion mt
+
+
+shit = MaqT ["q0", "q1"] "q0" "q0" "q1" "01" "01_"
+dshit = \state -> \input -> (state, '0', R)
+shittm = MT shit dshit
+
+-- | Gets the input alphabet from a Turing Machine
+sigma :: MT -> Alfabeto
+sigma = s . mtupla
+
+-- | Gets the tape alphabet from a Turing Machine
+gamma :: MT -> Alfabeto
+gamma = g . mtupla
+
+-- | Gets the state set from a Turing Machine
+estados :: MT -> [Estado]
+estados = q . mtupla
+
+-- | Gets the initial state from a Turing Machine
+estadoInicial :: MT -> Estado
+estadoInicial = q0 . mtupla
+
+-- | Gets the accepting state from a Turing Machine
+estadoAcept :: MT -> Estado
+estadoAcept = qf . mtupla
+
+-- | Gets the rejecting state from a Turing Machine
+estadoRechazo :: MT -> Estado
+estadoRechazo = qr . mtupla
+
+-- | Gets the transition function
+funTransicion :: MT -> Delta
+funTransicion = dltfun
+
 
 -- | Ejercicio 3, TM that accepts binary strings with even number of 0's
 tmPares :: MT
-tmPares = error "to be implemented..."
+tmPares = MT (MaqT states initState acceptState rejectState inpAlphabet tapeAlphabet) deltaFun
+  where
+    states = ["q" ++ [c] | c <- "01fr"]
+    initState = "q0"
+    acceptState = "qf"
+    rejectState = "qr"
+    inpAlphabet = "01"
+    tapeAlphabet = "01_"
+    deltaFun = \q -> \a -> case (q, a) of
+                             ("q0", '0') -> ("q1", '_', R)
+                             ("q0", '1') -> ("q0", '_', R)
+                             ("q1", '0') -> ("q0", '_', R)
+                             ("q1", '1') -> ("q1", '_', R)
+                             ("q0", '_') -> ("qf", '_', R)
+                             ("q1", '_') -> ("qr", '_', R)
+                             (qfinal, _) -> (qfinal, '_', R)
+
+
+-- | Flips 0s to 1s and 1s to 0s
+-- | Accepts (01)+0
+flippingTM :: MT
+flippingTM = MT (MaqT states initState acceptState rejectState inpAlphabet tapeAlphabet) deltaFun
+  where
+    states = ["q" ++ [c] | c <- "01fr"]
+    initState = "q0"
+    acceptState = "qf"
+    rejectState = "qr"
+    inpAlphabet = "01"
+    tapeAlphabet = "01_"
+    deltaFun = \q -> \a -> case (q, a) of
+                             ("q0", '0') -> ("q1", '1', R)
+                             ("q1", '1') -> ("q0", '0', R)
+                             ("q1", '_') -> ("qf", '_', R)
+                             (_, c) -> ("qr", c, R)
+
 
 -- | Ejercicio 4, TM for a^n b^n c^n
 tmanbncn :: MT
-tmanbncn = error "to be implemented..."
+tmanbncn = MT (MaqT states initState acceptState rejectState inpAlphabet tapeAlphabet) deltaFun
+  where
+    states = ["q" ++ [c] | c <- "123456789"] ++ ["s", "q10", "t", "r"]
+    initState = "s"
+    acceptState = "t"
+    rejectState = "r"
+    inpAlphabet = "abc"
+    tapeAlphabet = "abc[_]"
+    deltaFun = \q -> \a -> case (q, a) of
+                             -- first phase
+                             ("s", '[') -> ("s", '[', R)
+                             ("s", 'a') -> ("s", 'a', R)
+                             ("s", 'b') -> ("q1", 'b', R)
+                             ("s", 'c') -> ("q2", 'c', R)
+                             ("s", '_') -> ("q3", ']', L)
+                             ("s", ']') -> ("r", ']', R)
+                             ("q1", '[') -> ("r", '[', R)
+                             ("q1", 'a') -> ("r", 'a', R)
+                             ("q1", 'b') -> ("q1", 'b', R)
+                             ("q1", 'c') -> ("q2", 'c', R)
+                             ("q1", '_') -> ("q3", ']', L)
+                             ("q1", ']') -> ("r", ']', R)
+                             ("q2", '[') -> ("r", '[', R)
+                             ("q2", 'a') -> ("r", 'a', R)
+                             ("q2", 'b') -> ("r", 'b', R)
+                             ("q2", 'c') -> ("q2", 'c', R)
+                             ("q2", '_') -> ("q3", ']', L)
+                             ("q2", ']') -> ("r", ']', R)
+                             -- second phase
+                             ("q3", '[') -> ("t", '[', R)
+                             ("q3", 'a') -> ("r", 'a', R)
+                             ("q3", 'b') -> ("r", 'b', R)
+                             ("q3", 'c') -> ("q4", 'c', L)
+                             ("q3", '_') -> ("q3", '_', L)
+                             ("q3", ']') -> ("r", ']', R)
+                             ("q4", '[') -> ("r", '[', R)
+                             ("q4", 'a') -> ("r", 'a', R)
+                             ("q4", 'b') -> ("q5", '_', L)
+                             ("q4", 'c') -> ("q4", 'c', L)
+                             ("q4", '_') -> ("q4", '_', L)
+                             ("q4", ']') -> ("r", ']', R)
+                             ("q5", '[') -> ("r", '[', R)
+                             ("q5", 'a') -> ("q6", '_', L)
+                             ("q5", 'b') -> ("q5", 'b', L)
+                             ("q5", 'c') -> ("r", 'c', R)
+                             ("q5", '_') -> ("q5", '_', L)
+                             ("q5", ']') -> ("r", ']', R)
+                             ("q6", '[') -> ("q7", '[', R)
+                             ("q6", 'a') -> ("q6", 'a', L)
+                             ("q6", 'b') -> ("r", 'b', R)
+                             ("q6", 'c') -> ("r", 'c', R)
+                             ("q6", '_') -> ("q6", '_', L)
+                             ("q6", ']') -> ("r", ']', R)
+                             -- third phase
+                             ("q7", '[') -> ("r", '[', R)
+                             ("q7", 'a') -> ("q8", '_', R)
+                             ("q7", 'b') -> ("r", 'b', R)
+                             ("q7", 'c') -> ("r", 'c', R)
+                             ("q7", '_') -> ("q7", '_', R)
+                             ("q7", ']') -> ("t", ']', R)
+                             ("q8", '[') -> ("r", '[', R)
+                             ("q8", 'a') -> ("q8", 'a', R)
+                             ("q8", 'b') -> ("q9", '_', R)
+                             ("q8", 'c') -> ("r", 'c', R)
+                             ("q8", '_') -> ("q8", '_', R)
+                             ("q8", ']') -> ("r", ']', R)
+                             ("q9", '[') -> ("r", '[', R)
+                             ("q9", 'a') -> ("r", 'a', R)
+                             ("q9", 'b') -> ("q9", 'b', R)
+                             ("q9", 'c') -> ("q10", '_', R)
+                             ("q9", '_') -> ("q9", '_', R)
+                             ("q9", ']') -> ("r", ']', R)
+                             ("q10", '[') -> ("r", '[', R)
+                             ("q10", 'a') -> ("r", 'a', R)
+                             ("q10", 'b') -> ("r", 'b', R)
+                             ("q10", 'c') -> ("q10", 'c', R)
+                             ("q10", '_') -> ("q10", '_', R)
+                             ("q10", ']') -> ("q3", ']', L)
+                             -- accepting and rejecting states
+                             ("t", c) -> ("t", c, R)
+                             ("r", c) -> ("r", c, R)
+
 
 -- | Configuration-useful types
 type Cadena = [Simbolo]
@@ -106,18 +257,33 @@ sustituye (w:ws) n a
 
 -- | Ejercicio 5, delta function
 delta :: Delta -> Configuracion -> Configuracion
-delta = error "to be implemented..."
+delta d (q, w, n)
+  | n == (length w) - 1 = case d q nthSymb of
+                      (p, s, L) -> (p, subs s, n - 1)
+                      (p, s, R) -> (p, subs s ++ "_", n + 1)
+  | n == 0 = case d q nthSymb of
+               (p, s, L) -> (p, subs s, 0)
+               (p, s, R) -> (p, subs s, 1)
+  | otherwise = case d q nthSymb of
+                  (p, s, L) -> (p, subs s, n - 1)
+                  (p, s, R) -> (p, subs s, n + 1)
+  where
+    nthSymb = w !! n
+    subs s = sustituye w n s
+
 
 -- | Closure for delta function
 deltaEstrella :: MT -> Configuracion -> Bool
-deltaEstrella (mt, d) (q, w, n)
-  | q == qf mt = True
-  | q == qr mt = False
-  | otherwise = deltaEstrella (mt, d) (delta d (q,w,n))
+deltaEstrella mt (q, w, n)
+  | q == estadoAcept mt = True
+  | q == estadoRechazo mt = False
+  | otherwise = deltaEstrella mt (delta d (q,w,n))
+  where
+    d = funTransicion mt
 
 -- | Ejercicio 6, decides if the given string is accepted by the given TM
 aceptaCadena :: MT -> Cadena -> Bool
-aceptaCadena = error "to be implemented..."
+aceptaCadena mt string = deltaEstrella mt (estadoInicial mt, string, 0)
 
 -- | Auxiliar for Kleene star operation
 kln :: Alfabeto -> Int -> [Cadena]
